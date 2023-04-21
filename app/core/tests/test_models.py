eae16c797566f332560ab3f48766dfecaf4c2b2e
prashant5nov <prashantvelocity123@gmail.com>
2023-04-21 22:19:40 +0530

    user model and test cases

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,7 +4,7 @@ Test the models and functionality related to the models.
 
 
 from django.test import TestCase
-from ..models import User
+from django.contrib.auth import get_user_model
 
 
 class TestModel(TestCase):
@@ -13,8 +13,55 @@ class TestModel(TestCase):
 
         email = "test@example.com"
         password = "testpass123"
-        user = User.objects.create_user(
+
+        user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
         self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is in normalized format for any new user"""
+
+        password = "password@321"
+        sample_emails = [
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.COM", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, password)
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email address raises
+        ValueError
+
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                "",
+                "password@321"
+            )
+
+        """
+
+        self.assertRaises(
+            ValueError,
+            get_user_model().objects.create_user,
+            "",
+            "password@321"
+        )
+
+    def test_create_super_user(self):
+        """Test creating a superuser"""
+
+        user = get_user_model().objects.create_superuser(
+            "test@example.com",
+            "password@321"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
