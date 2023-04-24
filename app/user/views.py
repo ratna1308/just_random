1cf81a903000dd5ea4acc33f643bbe4e3dc81c39
prashant5nov <prashantvelocity123@gmail.com>
2023-04-24 21:35:46 +0530

    get, put, patch

--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
 from .serializers import UserSerializer, AuthTokenSerializer
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework import authentication, permissions
 
 # Create your views here.
 
@@ -22,3 +23,33 @@ class CreateTokenView(ObtainAuthToken):
     """
 
     serializer_class = AuthTokenSerializer
+
+
+class ManageUserView(RetrieveUpdateAPIView):
+    """
+    get
+    patch
+    put
+
+    TODO
+    TOPIC - generic views in DRF for get/put/patch (RetrieveUpdateAPIView)
+    refer -
+    https://www.django-rest-framework.org/api-guide/generic-views/#retrieveupdateapiview
+    """
+
+    serializer_class = UserSerializer
+
+    authentication_classes = [authentication.TokenAuthentication]
+
+    # TODO - refer
+    # https://www.django-rest-framework.org/api-guide/permissions/#setting-the-permission-policy
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """retrieve user and return authenticated user information
+
+        TODO - refer
+        https://www.django-rest-framework.org/api-guide/permissions/#object-level-permissions
+        """
+
+        return self.request.user
